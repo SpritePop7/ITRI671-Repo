db3c757b8f8f56589964e4753dcbb1f7709a3dcf
SpritePop7 <27694062@student365.msfed.nwu.ac.za>
2021-10-29 16:31:49 +0200

    Initial block-chain back-end files

--- Blockchain-backend/blockchain.py
+++ Blockchain-backend/blockchain.py
@@ -1,1 +1,50 @@
-PREP
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  6 00:55:36 2021
+
+@author: sprite
+"""
+
+# Creating the blockchain
+
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+from flask.wrappers import Request
+from flask import request
+import qrcode
+from PIL import Image
+import base64
+import pymongo
+import bcrypt
+
+
+# development
+
+# Part 1 - build the blockchain
+
+class Blockchain:
+    # needed componenets: genesisblock, chain, createblock, checks
+
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof=1, prev_Hash='0', hashcode='0', imgQRcode=0, site= "")
+
+    def create_block(self, proof, prev_Hash, hashcode, imgQRcode, site):
+        if site == "":
+            site = "No URL given"
+        
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': str(datetime.datetime.now()),
+            'proof': proof,
+            'prev_hash': prev_Hash,
+            'Data': hashcode,
+            'NftRawQR': imgQRcode,
+            'site': site
+        }
+        self.chain.append(block)
+        return block
+
--- Blockchain-backend/userBlockchain.py
+++ Blockchain-backend/userBlockchain.py
@@ -1,1 +1,39 @@
-PREP
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  7 21:30:36 2021
+
+@author: sprite
+"""
+
+import hashlib
+import json
+from flask import Flask, jsonify
+from flask.wrappers import Request
+from flask import request
+import base64
+import bcrypt
+import datetime
+
+class userBlockchain:
+
+
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof=1, prev_Hash='0', username="GenesisNull", password=self.hash("GenesisNullPass"))
+
+    def create_block(self, proof, prev_Hash, username, password):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': str(datetime.datetime.now()),
+            'proof': proof,
+            'prev_hash': prev_Hash,
+            'username': username,
+            'password': password
+        }
+        self.chain.append(block)
+        return block
+
+    def get_prev_block(self):
+        return self.chain[-1]
+
